254305c8167044034a33b639532f65f91056d87d
anggasaeful7 <anggasaeful3@gmail.com>
2023-12-07 17:24:35 +0700

    first commit

---
+++ models/UserModel.js
@@ -0,0 +1,67 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+
+const Users = db.define(
+  "users",
+  {
+    nama: {
+      type: DataTypes.STRING,
+    },
+    skpd: {
+      type: DataTypes.STRING,
+    },
+    ktp: {
+      type: DataTypes.STRING,
+    },
+    nip: {
+      type: DataTypes.STRING,
+    },
+    foto: {
+      type: DataTypes.STRING,
+    },
+    alamat: {
+      type: DataTypes.STRING,
+    },
+    no_hp: {
+      type: DataTypes.STRING,
+    },
+    unit_kerja: {
+      type: DataTypes.STRING,
+    },
+    jabatan: {
+      type: DataTypes.STRING,
+    },
+    hak_akses: {
+      type: DataTypes.STRING,
+    },
+    aktif: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    username: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: null,
+      field: "deleted_at",
+    },
+  },
+  {
+    paranoid: true,
+    freezeTableName: true,
+  }
+);
+
+export default Users;
